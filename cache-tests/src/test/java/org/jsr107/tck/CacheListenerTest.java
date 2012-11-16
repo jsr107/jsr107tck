dae16015defb40c44d661fed154b8cc8aa534191
gregrluck <gluck@gregluck.com>
2012-11-16 17:41:59 +1000

    More listener implementation and tests

--- cache-tests/src/test/java/org/jsr107/tck/CacheListenerTest.java
+++ cache-tests/src/test/java/org/jsr107/tck/CacheListenerTest.java
@@ -17,42 +17,25 @@
 
 package org.jsr107.tck;
 
-import domain.Beagle;
-import domain.Identifier;
-import manager.CacheNameOnEachMethodBlogManagerImpl;
 import org.jsr107.tck.util.ExcludeListExcluder;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.MethodRule;
 
-import javax.cache.Cache;
-import javax.cache.CacheConfiguration;
 import javax.cache.CacheManager;
-import javax.cache.Caching;
-import javax.cache.Status;
-import javax.cache.annotation.CacheRemoveAll;
+import javax.cache.event.CacheEntryCreatedListener;
 import javax.cache.event.CacheEntryEvent;
+import javax.cache.event.CacheEntryExpiredListener;
 import javax.cache.event.CacheEntryListenerException;
 import javax.cache.event.CacheEntryReadListener;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
+import javax.cache.event.CacheEntryRemovedListener;
+import javax.cache.event.CacheEntryUpdatedListener;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Unit tests for Cache Listeners.
@@ -99,12 +82,30 @@ public class CacheListenerTest extends CacheTestSupport<Long, String> {
      * Check the listener is getting reads
      */
     @Test
-    public void registerCacheEntryListener() {
+    public void testCacheEntryListener() {
         MyCacheEntryListener<Long, String> listener = new MyCacheEntryListener<Long, String>();
         cache.registerCacheEntryListener(listener, false, null, true);
+        assertEquals(0, listener.getCreated());
+        assertEquals(0, listener.getUpdated());
+        assertEquals(0, listener.getReads());
+        assertEquals(0, listener.getExpired());
+        assertEquals(0, listener.getRemoved());
+
         cache.put(1l, "dog");
+        assertEquals(1, listener.getCreated());
+        assertEquals(0, listener.getUpdated());
+        assertEquals(0, listener.getReads());
+        assertEquals(0, listener.getExpired());
+        assertEquals(0, listener.getRemoved());
+
         cache.get(1l);
+        assertEquals(1, listener.getCreated());
+        assertEquals(0, listener.getUpdated());
         assertEquals(1, listener.getReads());
+        assertEquals(0, listener.getExpired());
+        assertEquals(0, listener.getRemoved());
+
+
     }
 
 
@@ -119,7 +120,6 @@ public class CacheListenerTest extends CacheTestSupport<Long, String> {
 
 
 
-    // ---------- utilities ----------
 
     /**
      * Test listener
@@ -127,30 +127,64 @@ public class CacheListenerTest extends CacheTestSupport<Long, String> {
      * @param <K>
      * @param <V>
      */
-    static class MyCacheEntryListener<K, V> implements CacheEntryReadListener<K, V> {
+    static class MyCacheEntryListener<K, V> implements CacheEntryReadListener<K, V>, CacheEntryCreatedListener<K, V>,
+            CacheEntryUpdatedListener<K, V>, CacheEntryExpiredListener<K, V>, CacheEntryRemovedListener<K, V> {
 
         AtomicInteger reads = new AtomicInteger();
+        AtomicInteger created = new AtomicInteger();
+        AtomicInteger updated = new AtomicInteger();
+        AtomicInteger removed = new AtomicInteger();
+        AtomicInteger expired = new AtomicInteger();
 
         ArrayList<CacheEntryEvent<K,V>> entries = new ArrayList<CacheEntryEvent<K, V>>();
-
-
-        /**
-         * Called after the entry has been read. If no entry existed for the key the event is not called.
-         * This method is not called if a batch operation was performed.
-         *
-         * @param event The event just read.
-         */
-        @Override
-        public void entryRead(CacheEntryEvent<? extends K, ? extends V> event) {
-            reads.incrementAndGet();
-        }
 
         public int getReads() {
             return reads.get();
         }
 
+        public int getCreated() {
+            return created.get();
+        }
+
+        public int getUpdated() {
+            return updated.get();
+        }
+
+        public int getRemoved() {
+            return removed.get();
+        }
+
+        public int getExpired() {
+            return expired.get();
+        }
+
         public ArrayList<CacheEntryEvent<K, V>> getEntries() {
             return entries;
+        }
+
+        @Override
+        public void onCreated(Iterable iterable) throws CacheEntryListenerException {
+            created.incrementAndGet();
+        }
+
+        @Override
+        public void onExpired(Iterable iterable) throws CacheEntryListenerException {
+            expired.incrementAndGet();
+        }
+
+        @Override
+        public void onRemoved(Iterable iterable) throws CacheEntryListenerException {
+            removed.incrementAndGet();
+        }
+
+        @Override
+        public void onUpdated(Iterable iterable) throws CacheEntryListenerException {
+            updated.incrementAndGet();
+        }
+
+        @Override
+        public void onRead(Iterable<CacheEntryEvent<? extends K, ? extends V>> cacheEntryEvents) throws CacheEntryListenerException {
+            reads.incrementAndGet();
         }
     }
 }
