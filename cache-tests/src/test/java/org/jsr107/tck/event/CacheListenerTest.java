3c1fceccc21aabab66370da32d8b15651e9d05fd
Greg Luck <gluck@gregluck.com>
2013-08-21 14:37:40 +1000

    #224 Implement wrapping in listeners

--- cache-tests/src/test/java/org/jsr107/tck/event/CacheListenerTest.java
+++ cache-tests/src/test/java/org/jsr107/tck/event/CacheListenerTest.java
@@ -30,14 +30,11 @@ import javax.cache.CacheManager;
 import javax.cache.configuration.FactoryBuilder;
 import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
 import javax.cache.configuration.MutableConfiguration;
-import javax.cache.event.CacheEntryCreatedListener;
-import javax.cache.event.CacheEntryEvent;
-import javax.cache.event.CacheEntryExpiredListener;
-import javax.cache.event.CacheEntryListenerException;
-import javax.cache.event.CacheEntryRemovedListener;
-import javax.cache.event.CacheEntryUpdatedListener;
+import javax.cache.event.*;
 import javax.cache.expiry.Duration;
 import javax.cache.expiry.ModifiedExpiryPolicy;
+import javax.management.JMException;
+import java.io.IOError;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -141,12 +138,6 @@ public class CacheListenerTest extends CacheTestSupport<Long, String> {
     }
   }
 
-
-
-
-
-
-
   /**
    * Check the listener is getting reads
    */
@@ -228,6 +219,87 @@ public class CacheListenerTest extends CacheTestSupport<Long, String> {
     assertEquals(5, listener.getCreated());
     assertEquals(5, listener.getUpdated());
     assertEquals(5, listener.getRemoved());
+  }
+
+  /**
+   * Check the listener is only throwing CacheException
+   */
+  @Test
+  public void testBrokenCacheEntryListener() {
+
+    //setup
+    MyBrokenCacheEntryListener<Long, String> listener = new MyBrokenCacheEntryListener<Long, String>();
+    MutableCacheEntryListenerConfiguration<Long,String> listenerConfiguration
+     = new MutableCacheEntryListenerConfiguration<Long, String>(FactoryBuilder.factoryOf(listener), null, false, true);
+    cache.registerCacheEntryListener(listenerConfiguration);
+
+    try {
+      cache.put(1l, "Sooty");
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    Map<Long, String> entries = new HashMap<Long, String>();
+    entries.put(2l, "Lucky");
+    entries.put(3l, "Prince");
+
+    try {
+      cache.put(1l, "Sooty");
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    try {
+      cache.putAll(entries);
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    try {
+      cache.put(1l, "Sooty");
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    try {
+      cache.putAll(entries);
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    try {
+      cache.getAndPut(4l, "Cody");
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    try {
+      cache.getAndPut(4l, "Cody");
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    String value = cache.get(1l);
+    Cache.EntryProcessor<Long, String, String> multiArgEP = new MultiArgumentEntryProcessor<Long, String, String>();
+    try {
+      String result = cache.invoke(1l, multiArgEP, "These", "are", "arguments");
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
+    try {
+      String result = cache.invoke(1l, new SetEntryProcessor<Long, String,
+          String>("Zoot"));
+
+      Iterator<Cache.Entry<Long, String>> iterator = cache.iterator();
+      while (iterator.hasNext()) {
+        iterator.next();
+        iterator.remove();
+      }
+    } catch (CacheEntryListenerException e) {
+      //expected
+    }
+
   }
 
   /**
@@ -478,4 +550,68 @@ public class CacheListenerTest extends CacheTestSupport<Long, String> {
       }
     }
   }
+
+  /**
+   * Test listener which throws all sorts of Throwables.
+   *
+   * @param <K>
+   * @param <V>
+   */
+  static class MyBrokenCacheEntryListener<K, V> implements CacheEntryCreatedListener<K,
+      V>,
+      CacheEntryUpdatedListener<K, V>, CacheEntryExpiredListener<K, V>,
+      CacheEntryRemovedListener<K, V>, Serializable {
+
+    AtomicInteger created = new AtomicInteger();
+    AtomicInteger updated = new AtomicInteger();
+    AtomicInteger removed = new AtomicInteger();
+
+    ArrayList<CacheEntryEvent<K, V>> entries = new ArrayList<CacheEntryEvent<K, V>>();
+
+    public int getCreated() {
+      return created.get();
+    }
+
+    public int getUpdated() {
+      return updated.get();
+    }
+
+    public int getRemoved() {
+      return removed.get();
+    }
+
+    public ArrayList<CacheEntryEvent<K, V>> getEntries() {
+      return entries;
+    }
+
+    @Override
+    public void onCreated(Iterable<CacheEntryEvent<? extends K, ? extends V>> events) throws CacheEntryListenerException {
+      for (CacheEntryEvent<? extends K, ? extends V> event : events) {
+        assertEquals(CREATED, event.getEventType());
+        throw new CacheEntryListenerException("I broke");
+      }
+    }
+
+    @Override
+    public void onExpired(Iterable<CacheEntryEvent<? extends K, ? extends V>> events) throws CacheEntryListenerException {
+      //SKIP: we don't count expiry events as they can occur asynchronously
+    }
+
+    @Override
+    public void onRemoved(Iterable<CacheEntryEvent<? extends K, ? extends V>> events) throws CacheEntryListenerException {
+      for (CacheEntryEvent<? extends K, ? extends V> event : events) {
+        assertEquals(REMOVED, event.getEventType());
+        throw new IOError(null);
+      }
+    }
+
+    @Override
+    public void onUpdated(Iterable<CacheEntryEvent<? extends K, ? extends V>> events) throws CacheEntryListenerException {
+      for (CacheEntryEvent<? extends K, ? extends V> event : events) {
+        assertEquals(UPDATED, event.getEventType());
+        throw new UnsupportedOperationException();
+      }
+    }
+  }
+
 }
