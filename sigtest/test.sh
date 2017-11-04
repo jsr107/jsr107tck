5b40913ca19f6209edf55ab8c1e2b7229b29038a
Greg Luck <gluck@gregluck.com>
2017-11-04 10:54:07 -0700

    SigTest updated comments for 1.1.0

--- sigtest/test.sh
+++ sigtest/test.sh
@@ -3,5 +3,10 @@
 # Tests a version of the API against the 1.0.0 signature file
 # Author: Greg Luck
 # Date: 13 December 2013
+#
+# 1.0.0 Sig File tested against 1.1.0 final. Passes. Requires Java 8.
+# Tester: Vasillis Bekiaris
+# Date: 4 November 2017
+# STATUS:Passed
 
 java -jar sigtestdev.jar Test -Classpath ../../jsr107spec/target/cache-api-1.1.0-SNAPSHOT.jar:$JAVA_HOME/jre/lib/rt.jar -ApiVersion 1.0.0 -Package javax.cache -Filename jcache_1.0.0.sig -Mode bin -Static -Backward
